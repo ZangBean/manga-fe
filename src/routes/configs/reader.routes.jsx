7a28f9ef8a8a2dc3568f6ab437bb4a0e8c2628f5
zangbin <zangb@GIANG>
2025-12-15 16:42:36 +0700

    2025/12/15: page genre

--- src/routes/configs/reader.routes.jsx
+++ src/routes/configs/reader.routes.jsx
@@ -4,12 +4,14 @@ import { ROUTER_PATH } from '@/constants/routers'
 import ReaderLayout from '@/layouts/reader/ReaderLayout'
 
 const HomePage = lazy(() => import('@/pages/reader/HomePage'))
+const GenrePage = lazy(() => import('@/pages/reader/GenrePage'))
 const NewsPage = lazy(() => import('@/pages/reader/NewsPage'))
 const ContactPage = lazy(() => import('@/pages/reader/ContactPage'))
 
 const ReaderRoutes = (
   <Route element={<ReaderLayout />}>
     <Route path={ROUTER_PATH.HOME_PAGE.PATH} element={<HomePage />} />
+    <Route path={ROUTER_PATH.GENRE_PAGE.PATH} element={<GenrePage />} />
     <Route path={ROUTER_PATH.NEWS_PAGE.PATH} element={<NewsPage />} />
     <Route path={ROUTER_PATH.CONTACT_PAGE.PATH} element={<ContactPage />} />
   </Route>
