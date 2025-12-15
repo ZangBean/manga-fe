284b1fae4997722542a6719f1091055ad437031d
zangbin <zangb@GIANG>
2025-12-15 16:31:32 +0700

    2025/12/15: contact page

--- src/routes/configs/reader.routes.jsx
+++ src/routes/configs/reader.routes.jsx
@@ -5,11 +5,13 @@ import ReaderLayout from '@/layouts/reader/ReaderLayout'
 
 const HomePage = lazy(() => import('@/pages/reader/HomePage'))
 const NewsPage = lazy(() => import('@/pages/reader/NewsPage'))
+const ContactPage = lazy(() => import('@/pages/reader/ContactPage'))
 
 const ReaderRoutes = (
   <Route element={<ReaderLayout />}>
     <Route path={ROUTER_PATH.HOME_PAGE.PATH} element={<HomePage />} />
     <Route path={ROUTER_PATH.NEWS_PAGE.PATH} element={<NewsPage />} />
+    <Route path={ROUTER_PATH.CONTACT_PAGE.PATH} element={<ContactPage />} />
   </Route>
 )
 
